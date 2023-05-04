17bc3ca47ce94eb135bdb8ceb40dce354ebfe0fc
Кохинець Анастасія <131793723+Akozynetsj@users.noreply.github.com>
2023-05-04 20:21:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ class Student:
     def __init__(self, name, age):
         self.name = name
         self.age = age
-    def info(selfself):
+    def info(self):
         print(f'Ім'я: {self.name}, вік: {self.age}')
 student = Student('Vasia', 23)
 student.info()
