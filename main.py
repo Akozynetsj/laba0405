ae17cd8e637ade85dcd669b84d2e9fb1e93472f0
Akozynetsj <Kozin_dn78@student.itstep.org>
2023-05-04 20:18:40 +0300

    ex 2

--- main.py
+++
@@ -1,9 +0,0 @@
-#1
-class Student:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-    def info(selfself):
-        print(f'Ім'я: {self.name}, вік: {self.age}')
-student = Student('Vasia', 23)
-student.info()
